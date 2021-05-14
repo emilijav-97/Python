0bc2217b1eab6ea0e192cd13a7873632b856de89
EmilijaV <76113883+emilijav-97@users.noreply.github.com>
2021-05-14 12:56:52 +0300

    Update TestBench_memory.v

--- Python+Verilog project/Verilog with Vivado HL WebPACK/TestBench_memory.v
+++ Python+Verilog project/Verilog with Vivado HL WebPACK/TestBench_memory.v
@@ -1,25 +1,35 @@
-module memory_python_tb();
-	parameter N = 4;
-	reg [N -1:0] address;
-	reg read_enable;
-	wire [N -1:0] data;
-	integer i;
-
-	memory_python #(N) inst( .read_en(read_en),
-				 .data(data), 
-				 .address(address, 
-				 .read_enable(read_enable));
+`timescale 1ns / 1ps
+ module mempy_TB;
+ parameter N = 2;
+ reg [(2*N) - 1:0] address;
+ reg read_en, en;
+ wire [(2*N) - 1:0] data;
  
+integer i,j;
+  
+memory_code dut(.address(address),
+          .data(data),
+          .read_en(read_en),
+          .en(en));
+
 initial begin
-	address = 0;
-   	read_en = 0;
-   	#10 $monitor ("address = %b, data = %b, read_enable = %b", address, data, read_enable);
-   
-	for (i = 0; i <(2**n); i = i +1)
-		begin
-			read_enable = 0;
-     			#10 address = i;
-     			read_enable = 1;
-   		end
-end
-endmodule
+    address = 0;
+    read_en = 0;
+    en      = 0;
+    #10 $monitor ("address = %h, data = %h, read_en = %b, en = %b", address, data, read_en, en);
+
+    for (i = 0; i < 2**(2*N); i = i + 1 )begin
+        for (j = 0; j < 2**(2*N); j = j + 1)begin
+            
+            #5 address = {i,j};
+             read_en = 1;
+             en = 1;
+             #5 read_en = 0;
+             en = 0;
+             address = 0;
+            
+             end
+         end
+    end
+ 
+ endmodule
