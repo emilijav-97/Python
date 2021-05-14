da925e574c29bb09791ec3af3777a6d7e13446d5
EmilijaV <76113883+emilijav-97@users.noreply.github.com>
2021-05-14 12:49:50 +0300

    Update multiplication_tb.v

--- Python+Verilog project/multiplication_tb.v
+++ Python+Verilog project/multiplication_tb.v
@@ -1,36 +1,36 @@
 
-    `timescale 1ns / 1ps
-    module mempy_TB;
-    parameter N = 2;
-    reg [(2*N) - 1:0] address;
-    reg read_en, en;
-    wire [(2*N) - 1:0] data;
+`timescale 1ns / 1ps
+ module mempy_TB;
+ parameter N = 2;
+ reg [(2*N) - 1:0] address;
+ reg read_en, en;
+ wire [(2*N) - 1:0] data;
  
-    integer i,j;
+integer i,j;
   
 memory_code dut(.address(address),
           .data(data),
           .read_en(read_en),
           .en(en));
 
- initial begin
+initial begin
+    address = 0;
+    read_en = 0;
+    en      = 0;
+    #10 $monitor ("address = %h, data = %h, read_en = %b, en = %b", address, data, read_en, en);
 
-   address = 0;
-   read_en = 0;
-   en      = 0;
-
-   #10 $monitor ("address = %h, data = %h, read_en = %b, en = %b", address, data, read_en, en);
-
-   for (i = 0; i < 2**(2*N); i = i + 1 )begin
-     for (j = 0; j < 2**(2*N); j = j + 1)begin
-
-     #5 address = {i,j};
-     read_en = 1;
-     en = 1;
-     #5 read_en = 0;
-     en = 0;
-     address = 0;
+    for (i = 0; i < 2**(2*N); i = i + 1 )begin
+        for (j = 0; j < 2**(2*N); j = j + 1)begin
+            
+            #5 address = {i,j};
+             read_en = 1;
+             en = 1;
+             #5 read_en = 0;
+             en = 0;
+             address = 0;
+            
+             end
+         end
     end
-  end
-end
-endmodule
+ 
+ endmodule
