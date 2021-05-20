73502200908d5b477e991b3e634b3ed57fcd27bb
EmilijaV <76113883+emilijav-97@users.noreply.github.com>
2021-05-20 13:22:58 +0300

    Update test.script.py

--- Python+Verilog project/Python Scripts/test.script.py
+++ Python+Verilog project/Python Scripts/test.script.py
@@ -1,6 +1,7 @@
+
 # reading files
-f1 = open("/home/emilija/Desktop/test/results.txt", "r")  
-f2 = open("/home/emilija/Desktop/test/rom_16x4.dat", "r")  
+f1 = open("/home/emilija/Desktop/test/new1.txt", "r")  
+f2 = open("/home/emilija/Desktop/test/new2.txt", "r")  
 f = open("results_compared.txt","w+")  
 i = 0
   
@@ -11,19 +12,18 @@ for line1 in f1:
           
         # matching line1 from both files
         if line1 == line2:  
-     
-            # print IDENTICAL if similar
             print("Line ", i, ": IDENTICAL") 
-            f.write("Identical")
+            f.write("Identical \n ") 
+            
                 
         else:
             print("Line ", i, ":")
             # else print that line from both files
             print("\tFile 1:", line1, end='')
             print("\tFile 2:", line2, end='')
-            f.write("Different: \n " )
-            f.write(f"Line {i})--> {line1} ")
-            f.write(f"Line {i})--> {line2} \n ")
+            f.write(f"Different found in Line {i}  \n " )
+            f.write(f"Line {i} in File1)--> {line1} ")
+            f.write(f"Line {i} in File2)--> {line2} \n ")
             
         break
         
